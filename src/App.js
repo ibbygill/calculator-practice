26db0a379b699886d8132f211373b6933e073871
Ibjyot Gill <igill47@my.bcit.ca>
2022-09-07 23:09:26 -0700

    still changing a few functions

--- src/App.js
+++ src/App.js
@@ -14,6 +14,13 @@ export const ACTIONS = {
 function reducer(state, {type, payload}){
   switch (type){
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false,
+        }
+      }
       if (payload.digit === "0" && state.currentOperand === "0") 
       return  state
       // if zero is already inputed this will not allow you to enter more 0s to the starting value
@@ -53,6 +60,20 @@ function reducer(state, {type, payload}){
     case ACTIONS.CLEAR:
       return{}
       // removes all if you click AC
+    case ACTIONS.EVALUATE:
+      if (state.operation == null ||
+        state.currentOperand == null ||
+        state.previousOperand == null 
+        ) {
+          return state
+        }
+        return {
+          ...state,
+          overwrite: true,
+          previousOperand: null,
+          operation: null,
+          currentOperand: evaluate(state)
+        }
   }
 }
 
@@ -108,7 +129,10 @@ function App() {
       <OperationButton operation="-" dispatch={dispatch}/>
       <DigitButton digit="." dispatch={dispatch}/>
       <DigitButton digit="0" dispatch={dispatch}/>
-      <button className="span-two"> = </button>
+      <button 
+      className="span-two"
+      onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
+      > = </button>
     </div>
   )
 }
